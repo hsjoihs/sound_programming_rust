4963f8d3251b49d4c51cae29ecb14c51a99a67bb
hsjoihs <hs.ioling.hs@gmail.com>
2018-05-15 23:46:19 +0900

    remove unnecessary comments

--- sound_programming/src/main.rs
+++ sound_programming/src/main.rs
@@ -66,34 +66,7 @@ fn ex1_1(){
 	// pcm0.s possibly leaks? (may be handled nicely by drop of pcm0_slice)
 
 }
-// ex1_1.c:
-/*
-int main(void)
-{
-  MONO_PCM pcm0, pcm1;
-  int n;
-  
-  wave_read_16bit_mono(&pcm0, "a.wav");
-  
-  pcm1.fs = pcm0.fs; 
-  pcm1.bits = pcm0.bits; /* 量子化精度 */
-  pcm1.length = pcm0.length; /* 音データの長さ */
-  pcm1.s = calloc(pcm1.length, sizeof(double)); /* 音データ */
-  
-  for (n = 0; n < pcm1.length; n++)
-  {
-    pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
-  }
-  
-  wave_write_16bit_mono(&pcm1, "b.wav"); /* 音データの出力 */
-  
-  free(pcm0.s);
-  free(pcm1.s);
-  
-  return 0;
-}
 
-*/
 #[allow(non_snake_case)]
 fn ex1_2(){
 	unsafe{
@@ -122,43 +95,9 @@ fn ex1_2(){
 
 		wave_write_16bit_stereo(&mut pcm1, to_c_str("ex1_2_b.wav")); /* 音データの出力 */
 	}
-	// pcm0.sL and pcm0.sR leaks
+	// pcm0.sL and pcm0.sR possibly leaks?
 }
-// ex1_2.c:
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include "wave.h"
 
-int main(void)
-{
-  STEREO_PCM pcm0, pcm1;
-  int n;
-  
-  wave_read_16bit_stereo(&pcm0, "a.wav"); /* 音データの入力 */
-  
-  pcm1.fs = pcm0.fs; /* 標本化周波数 */
-  pcm1.bits = pcm0.bits; /* 量子化精度 */
-  pcm1.length = pcm0.length; /* 音データの長さ */
-  pcm1.sL = calloc(pcm1.length, sizeof(double)); /* 音データ（左） */
-  pcm1.sR = calloc(pcm1.length, sizeof(double)); /* 音データ（右） */
-  
-  for (n = 0; n < pcm1.length; n++)
-  {
-    pcm1.sL[n] = pcm0.sL[n]; /* 音データ（左） のコピー */
-    pcm1.sR[n] = pcm0.sR[n]; /* 音データ（右）のコピー */
-  }
-  
-  wave_write_16bit_stereo(&pcm1, "b.wav"); /* 音データの出力 */
-  
-  free(pcm0.sL);
-  free(pcm0.sR);
-  free(pcm1.sL);
-  free(pcm1.sR);
-  
-  return 0;
-}
-*/
 
 fn ex2_1(){
 
@@ -185,35 +124,7 @@ fn ex2_1(){
 	} 	
 }
 
-// ex2_1.c:
-/*
-int main(void)
-{
-  MONO_PCM pcm;
-  int n;
-  double a, f0;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  a = 0.1; /* 振幅 */
-  f0 = 500.0; /* 周波数 */
-  
-  /* サイン波 */
-  for (n = 0; n < pcm.length; n++)
-  {
-    pcm.s[n] = a * sin(2.0 * M_PI * f0 * n / pcm.fs);
-  }
-  
-  wave_write_16bit_mono(&pcm, "ex2_1.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-*/
+
 
 
 
@@ -267,67 +178,6 @@ fn itdyi (i : c_int, d: c_double) -> c_int {
 	((i as c_double) * d) as c_int
 }
 
-// ex2_2.c:
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-
-void sine_wave(MONO_PCM *pcm, double f0, double a, int offset, int duration)
-{
-  int n;
-  double *s;
-  
-  s = calloc(duration, sizeof(double));
-  
-  /* サイン波 */
-  for (n = 0; n < duration; n++)
-  {
-    s[n] = a * sin(2.0 * M_PI * f0 * n / pcm->fs);
-  }
-  
-  /* フェード処理 */
-  for (n = 0; n < pcm->fs*0.01; n++)
-  {
-    s[n] *= (double)n / (pcm->fs * 0.01);
-    s[duration - n - 1] *= (double)n / (pcm->fs * 0.01);
-  }
-  
-  for (n = 0; n < duration; n++)
-  {
-    pcm->s[offset + n] += s[n];
-  }
-  
-  free(s);
-}
-
-int main(void)
-{
-  MONO_PCM pcm;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 2; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  sine_wave(&pcm, 261.63, 0.1, pcm.fs * 0.00, pcm.fs * 0.25); /* C4 */
-  sine_wave(&pcm, 293.66, 0.1, pcm.fs * 0.25, pcm.fs * 0.25); /* D4 */
-  sine_wave(&pcm, 329.63, 0.1, pcm.fs * 0.50, pcm.fs * 0.25); /* E4 */
-  sine_wave(&pcm, 349.23, 0.1, pcm.fs * 0.75, pcm.fs * 0.25); /* F4 */
-  sine_wave(&pcm, 392.00, 0.1, pcm.fs * 1.00, pcm.fs * 0.25); /* G4 */
-  sine_wave(&pcm, 440.00, 0.1, pcm.fs * 1.25, pcm.fs * 0.25); /* A4 */
-  sine_wave(&pcm, 493.88, 0.1, pcm.fs * 1.50, pcm.fs * 0.25); /* B4 */
-  sine_wave(&pcm, 523.25, 0.1, pcm.fs * 1.75, pcm.fs * 0.25); /* C5 */
-  
-  wave_write_16bit_mono(&pcm, "ex2_2.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-*/
-
 
 fn ex3_1(){
 	let f0 = 500.0; /* 基本周波数 */
@@ -362,49 +212,6 @@ fn ex3_1(){
 		wave_write_16bit_mono(&mut pcm, to_c_str("ex3_1.wav"));
 	}
 }
-// ex3_1.c:
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, i;
-  double f0, gain;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  f0 = 500.0; /* 基本周波数 */
-  
-  /* ノコギリ波 */
-  for (i = 1; i <= 44; i++)
-  {
-    for (n = 0; n < pcm.length; n++)
-    {
-      pcm.s[n] += 1.0 / i * sin(2.0 * M_PI * i * f0 * n / pcm.fs);
-    }
-  }
-  
-  gain = 0.1; /* ゲイン */
-  
-  for (n = 0; n < pcm.length; n++)
-  {
-    pcm.s[n] *= gain;
-  }
-  
-  wave_write_16bit_mono(&pcm, "ex3_1.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-*/
 
 fn ex3_2(){
 	let f0 = 500.0; /* 基本周波数 */
@@ -440,50 +247,6 @@ fn ex3_2(){
 	}
 }
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, i;
-  double f0, gain;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  f0 = 500.0; /* 基本周波数 */
-  
-  /* 矩形波 */
-  for (i = 1; i <= 44; i = i + 2)
-  {
-    for (n = 0; n < pcm.length; n++)
-    {
-      pcm.s[n] += 1.0 / i * sin(2.0 * M_PI * i * f0 * n / pcm.fs);
-    }
-  }
-  
-  gain = 0.1; /* ゲイン */
-  
-  for (n = 0; n < pcm.length; n++)
-  {
-    pcm.s[n] *= gain;
-  }
-  
-  wave_write_16bit_mono(&pcm, "ex3_2.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-
-
-*/
 
 fn ex3_3(){
 	let f0 = 500.0; /* 基本周波数 */
@@ -518,49 +281,7 @@ fn ex3_3(){
 		wave_write_16bit_mono(&mut pcm, to_c_str("ex3_3.wav"));
 	}
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
 
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, i;
-  double f0, gain;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  f0 = 500.0; /* 基本周波数 */
-  
-  /* 三角波 */
-  for (i = 1; i <= 44; i = i + 2)
-  {
-    for (n = 0; n < pcm.length; n++)
-    {
-      pcm.s[n] += 1.0 / i / i * sin(M_PI * i / 2.0) * sin(2.0 * M_PI * i * f0 * n / pcm.fs);
-    }
-  }
-  
-  gain = 0.1; /* ゲイン */
-  
-  for (n = 0; n < pcm.length; n++)
-  {
-    pcm.s[n] *= gain;
-  }
-  
-  wave_write_16bit_mono(&pcm, "ex3_3.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-
-*/
 
 fn ex3_4(){
 	let f0 = 500.0; /* 基本周波数 */
@@ -595,50 +316,7 @@ fn ex3_4(){
 		wave_write_16bit_mono(&mut pcm, to_c_str("ex3_4.wav"));
 	}
 }
-// ex3_4.c
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
 
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, i;
-  double f0, gain;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  f0 = 500.0; /* 基本周波数 */
-  
-  /* コサイン波の重ね合わせによるノコギリ波 */
-  for (i = 1; i <= 44; i++)
-  {
-    for (n = 0; n < pcm.length; n++)
-    {
-      pcm.s[n] += 1.0 / i * cos(2.0 * M_PI * i * f0 * n / pcm.fs);
-    }
-  }
-  
-  gain = 0.1; /* ゲイン */
-  
-  for (n = 0; n < pcm.length; n++)
-  {
-    pcm.s[n] *= gain;
-  }
-  
-  wave_write_16bit_mono(&pcm, "ex3_4.wav");
-  
-  free(pcm.s);
-  
-  return 0;
-}
-
-*/
 
 fn ex3_5(){
 	let f0 = 1.0; /* 基本周波数 */
@@ -681,41 +359,6 @@ fn ex3_5(){
 	}
 }
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, i;
-  double f0, theta, gain;
-  
-  pcm.fs = 44100; /* 標本化周波数 */
-  pcm.bits = 16; /* 量子化精度 */
-  pcm.length = pcm.fs * 1; /* 音データの長さ */
-  pcm.s = calloc(pcm.length, sizeof(double)); /* 音データ */
-  
-  f0 = 1.0; /* 基本周波数 */
-  
-  /* 白色雑音 */
-  for (i = 1; i <= 22050; i++)
-  {
-    theta = (double)rand() / RAND_MAX * 2.0 * M_PI;
-    
-    for (n = 0; n < pcm.length; n++)
-    {
-      pcm.s[n] += sin(2.0 * M_PI * i * f0 * n / pcm.fs + theta);
-    }
-  }
-
-  
-
-}
-
-*/
 #[allow(non_snake_case)]
 fn verify(X_real: Vec<c_double>, X_imag: Vec<c_double>){
 	let N = 64;
@@ -797,71 +440,9 @@ fn ex4_2(){
 			61 => -4.0,
 			_ => 0.0
 		});
-    	//println!("X({}) = j{}\n", k, X_imag[k]);
   	}
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-#include "window_function.h"
 
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, k, N;
-  double *x_real, *x_imag, *X_real, *X_imag, *w, W_real, W_imag;
-  
-  wave_read_16bit_mono(&pcm, "sine_500hz.wav");
-  
-  N = 64; /* DFTのサイズ */
-  
-  x_real = calloc(N, sizeof(double));
-  x_imag = calloc(N, sizeof(double));
-  X_real = calloc(N, sizeof(double));
-  X_imag = calloc(N, sizeof(double));
-  w = calloc(N, sizeof(double));
-  
-  Hanning_window(w, N); /* ハニング窓 */
-  
-  /* 波形 */
-  for (n = 0; n < N; n++)
-  {
-    x_real[n] = pcm.s[n] * w[n]; /* x(n)の実数部 */
-    x_imag[n] = 0.0; /* x(n)の虚数部 */
-  }
-  
-  /* DFT */
-  for (k = 0; k < N; k++)
-  {
-    for (n = 0; n < N; n++)
-    {
-      W_real = cos(2.0 * M_PI * k * n / N);
-      W_imag = -sin(2.0 * M_PI * k * n / N);
-      X_real[k] += W_real * x_real[n] - W_imag * x_imag[n]; /* X(k)の実数部 */
-      X_imag[k] += W_real * x_imag[n] + W_imag * x_real[n]; /* X(k)の虚数部 */
-    }
-  }
-  
-  /* 周波数特性 */
-  for (k = 0; k < N; k++)
-  {
-    printf("X(%d) = %f+j%f¥n", k, X_real[k], X_imag[k]);
-  }
-  
-  free(pcm.s);
-  free(x_real);
-  free(x_imag);
-  free(X_real);
-  free(X_imag);
-  free(w);
-  
-  return 0;
-}
-
-
-*/
 
 #[allow(non_snake_case)]
 fn ex4_3(){
@@ -885,52 +466,8 @@ fn ex4_3(){
 
 }
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-#include "fft.h"
 
-int main(void)
-{
-  MONO_PCM pcm;
-  int n, k, N;
-  double *x_real, *x_imag;
-  
-  wave_read_16bit_mono(&pcm, "sine_500hz.wav");
-  
-  N = 64; /* DFTのサイズ */
-  
-  x_real = calloc(N, sizeof(double));
-  x_imag = calloc(N, sizeof(double));
-  
-  /* 波形 */
-  for (n = 0; n < N; n++)
-  {
-    x_real[n] = pcm.s[n]; /* x(n)の実数部 */
-    x_imag[n] = 0.0; /* x(n)の虚数部 */
-  }
-  
-  FFT(x_real, x_imag, N);
-  
-  /* 周波数特性 */
-  for (k = 0; k < N; k++)
-  {
-    printf("X(%d) = %f+j%f¥n", k, x_real[k], x_imag[k]);
-  }
-  
-  free(pcm.s);
-  free(x_real);
-  free(x_imag);
-  
-  return 0;
-}
-
-
-*/
-
-#[allow(non_snake_case, unused_mut, unused_variables)]
+#[allow(non_snake_case)]
 fn ex6_4(){
 	unsafe{
 		let mut pcm0 : MONO_PCM = mem::uninitialized();
