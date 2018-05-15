008ea7c0f99d640000322ef5afd3780142775050
hsjoihs <hs.ioling.hs@gmail.com>
2018-05-16 03:40:42 +0900

    ex5_5 successful

--- sound_programming/src/main.rs
+++ sound_programming/src/main.rs
@@ -35,6 +35,7 @@ fn main() {
 	ex5_2();
 	ex5_3();
 	ex5_4();
+	ex5_5();
 	ex6_4();
 	ex10_4();
 	assert_eq!(sinc(2.1),2.1f64.sin()/2.1 );
@@ -493,7 +494,7 @@ fn ex5_3(){
 
 }
 
-#[allow(non_snake_case, unused_variables)]
+#[allow(non_snake_case)]
 fn ex5_4(){
 	let pcm_fs = 44100; /* 標本化周波数 */
     let pcm_bits = 16; /* 量子化精度 */
@@ -533,26 +534,54 @@ fn ex5_4(){
   	wave_write_16bit_mono_safer2("ex5_4.wav", (&mut pcm_s, pcm_fs as i32, pcm_bits, pcm_length as i32));
 
 }
-/*
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "wave.h"
+#[allow(non_snake_case, unused_variables)]
+fn ex5_5(){
+	let pcm_fs = 44100; /* 標本化周波数 */
+    let pcm_bits = 16; /* 量子化精度 */
+    let pcm_length = pcm_fs * 4; /* 音データの長さ */
+    let mut pcm_s = vec![0.0; pcm_length]; /* 音データ */	
+    let mut a0 = vec![0.0; pcm_length];
+    let mut a1 = vec![0.0; pcm_length];
+    let mut a2 = vec![0.0; pcm_length];
+    let mut a3 = vec![0.0; pcm_length];
+    let mut a4 = vec![0.0; pcm_length];
+    let f0 = vec![440.0; pcm_length];
+    let f1 = vec![880.0; pcm_length];
+    let f2 = vec![1320.0; pcm_length];
+    let f3 = vec![1760.0; pcm_length];
+    let f4 = vec![2200.0; pcm_length];
+    /* 時間エンベロープ */
+  	for n in 0..pcm_length {
+    	a0[n] = 1.0 * (-5.0 * n as f64 / (pcm_fs as f64 * 4.0)).exp();
+    	a1[n] = 0.8 * (-5.0 * n as f64 / (pcm_fs as f64 * 2.0)).exp();
+    	a2[n] = 0.6 * (-5.0 * n as f64 / (pcm_fs as f64 * 1.0)).exp();
+    	a3[n] = 0.5 * (-5.0 * n as f64 / (pcm_fs as f64 * 0.5)).exp();
+    	a4[n] = 0.4 * (-5.0 * n as f64 / (pcm_fs as f64 * 0.2)).exp();
+  	}
+  	/* 加算合成 */
+    for n in 0..pcm_length {
+        pcm_s[n] += a0 [n] * (2.0 * PI * f0[n] * n as f64 / pcm_fs as f64).sin();
+        pcm_s[n] += a1 [n] * (2.0 * PI * f1[n] * n as f64 / pcm_fs as f64).sin();
+        pcm_s[n] += a2 [n] * (2.0 * PI * f2[n] * n as f64 / pcm_fs as f64).sin();
+        pcm_s[n] += a3 [n] * (2.0 * PI * f3[n] * n as f64 / pcm_fs as f64).sin();
+        pcm_s[n] += a4 [n] * (2.0 * PI * f4[n] * n as f64 / pcm_fs as f64).sin();
+    }
 
-int main(void)
-{
-  
-  
-  
-  /* フェード処理 */
-  
-  
+    let gain = 0.1; /* ゲイン */
 
+    for n in 0..pcm_length {
+    	pcm_s[n] *= gain;
+    }
+
+    /* フェード処理 */
+  	for n in 0..(pcm_fs as f64*0.01).ceil() as usize  {
+    	pcm_s[n] *= n as f64 / (pcm_fs as f64 * 0.01);
+    	pcm_s[pcm_length - n - 1] *= n as f64 / (pcm_fs as f64 * 0.01);
+  	}
+
+  	wave_write_16bit_mono_safer2("ex5_5.wav", (&mut pcm_s, pcm_fs as i32, pcm_bits, pcm_length as i32));
 }
-
-
-*/
 
 
 #[allow(non_snake_case)]
