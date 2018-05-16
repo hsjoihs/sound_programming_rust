7c401ebcb77f1b6ef626ad370d2f96ffae02b29d
hsjoihs <hs.ioling.hs@gmail.com>
2018-05-16 11:34:37 +0900

    port iir_filter.c

--- sound_programming/build.rs
+++ sound_programming/build.rs
@@ -3,10 +3,7 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         .file("src/fir_filter.c")
-        .compile("libfir_filter.a");
-    cc::Build::new()
-        .file("src/iir_filter.c")
-        .compile("libiir_filter.a");      
+        .compile("libfir_filter.a");   
     cc::Build::new()
         .file("src/wave.c")
         .compile("libwave.a");    
--- sound_programming/src/filter.rs
+++ sound_programming/src/filter.rs
@@ -44,23 +44,121 @@ pub fn safe_FIR_filtering(x: &[c_double], y: &mut [c_double], L: usize, b: &mut 
 }
 
 
-#[link(name = "iir_filter")]
-extern {
-	 /*pub*/ //fn IIR_LPF(fc  : c_double, Q:  c_double , a: *mut c_double, b: *mut c_double)
-	pub fn IIR_HPF(fc  : c_double, Q:  c_double , a: *mut c_double, b: *mut c_double)
-	;pub fn IIR_BPF(fc1 : c_double, fc2: c_double, a: *mut c_double, b: *mut c_double)
-	;pub fn IIR_BEF(fc1 : c_double, fc2: c_double, a: *mut c_double, b: *mut c_double)
-	;/*pub*/ //fn IIR_resonator(fc: c_double, Q: c_double, a: *mut c_double, b: *mut c_double)
-	pub fn IIR_notch(fc: c_double, Q: c_double, a: *mut c_double, b: *mut c_double)
-	;pub fn IIR_low_shelving(fc: c_double, Q: c_double,g: c_double, a: *mut c_double, b: *mut c_double)
-	;pub fn IIR_high_shelving(fc: c_double, Q: c_double,g: c_double, a: *mut c_double, b: *mut c_double)
-	;pub fn IIR_peaking(fc: c_double, Q: c_double,g: c_double, a: *mut c_double, b: *mut c_double)
-	;//*pub*/ fn IIR_filtering(x: *const c_double, y: *mut c_double, L: c_int, a: *const c_double, b: *const c_double, I: c_int, J: c_int);
+
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_HPF(fc  : c_double, Q:  c_double , a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+  	let fc = (PI * fc).tan() / (2.0 * PI);	
+  	a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
+    a[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[0] = 1.0 / a[0];
+    b[1] = -2.0 / a[0];
+    b[2] = 1.0 / a[0];
+    
+    a[0] = 1.0;
+}
+#[allow(non_snake_case)]
+pub fn safe_IIR_BPF(fc1 : c_double, fc2: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+	let fc1 = (PI * fc1).tan() / (2.0 * PI);	
+	let fc2 = (PI * fc2).tan() / (2.0 * PI);	
+	a[0] = 1.0 + 2.0 * PI * (fc2 - fc1) + 4.0 * PI * PI * fc1 * fc2;
+    a[1] = (8.0 * PI * PI * fc1 * fc2 - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * (fc2 - fc1) + 4.0 * PI * PI * fc1 * fc2) / a[0];
+    b[0] = 2.0 * PI * (fc2 - fc1) / a[0];
+    b[1] = 0.0;
+    b[2] = -2.0 * PI * (fc2 - fc1) / a[0];
+    
+    a[0] = 1.0;
+}
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_BEF(fc1 : c_double, fc2: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+	let fc1 = (PI * fc1).tan() / (2.0 * PI);	
+	let fc2 = (PI * fc2).tan() / (2.0 * PI);
+	
+	a[0] = 1.0 + 2.0 * PI * (fc2 - fc1) + 4.0 * PI * PI * fc1 * fc2;
+    a[1] = (8.0 * PI * PI * fc1 * fc2 - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * (fc2 - fc1) + 4.0 * PI * PI * fc1 * fc2) / a[0];
+    b[0] = (4.0 * PI * PI * fc1 * fc2 + 1.0) / a[0];
+    b[1] = (8.0 * PI * PI * fc1 * fc2 - 2.0) / a[0];
+    b[2] = (4.0 * PI * PI * fc1 * fc2 + 1.0) / a[0];
+    
+    a[0] = 1.0;
+}
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_notch(fc: c_double, Q: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+  	let fc = (PI * fc).tan() / (2.0 * PI);	
+    a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
+    a[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[0] = (4.0 * PI * PI * fc * fc + 1.0) / a[0];
+    b[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    b[2] = (4.0 * PI * PI * fc * fc + 1.0) / a[0];
+  
+    a[0] = 1.0;	
+}
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_low_shelving(fc: c_double, Q: c_double,g: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+	let fc = (PI * fc).tan() / (2.0 * PI);
+	a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
+    a[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[0] = (1.0 + (1.0 + g).sqrt() * 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc * (1.0 + g)) / a[0];
+    b[1] = (8.0 * PI * PI * fc * fc * (1.0 + g) - 2.0) / a[0];
+    b[2] = (1.0 - (1.0 + g).sqrt() * 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc * (1.0 + g)) / a[0];
+    
+    a[0] = 1.0;	
+}
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_high_shelving(fc: c_double, Q: c_double,g: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+	let fc = (PI * fc).tan() / (2.0 * PI);
+    a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
+    a[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[0] = ((1.0 + g) + (1.0 + g).sqrt() * 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[1] = (8.0 * PI * PI * fc * fc - 2.0 * (1.0 + g)) / a[0];
+    b[2] = ((1.0 + g) - (1.0 + g).sqrt() * 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    
+    a[0] = 1.0;
+}
+
+
+#[allow(non_snake_case)]
+pub fn safe_IIR_peaking(fc: c_double, Q: c_double,g: c_double, a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
+	let fc = (PI * fc).tan() / (2.0 * PI);
+	a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
+    a[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    a[2] = (1.0 - 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc) / a[0];
+    b[0] = (1.0 + 2.0 * PI * fc / Q * (1.0 + g) + 4.0 * PI * PI * fc * fc) / a[0];
+    b[1] = (8.0 * PI * PI * fc * fc - 2.0) / a[0];
+    b[2] = (1.0 - 2.0 * PI * fc / Q * (1.0 + g) + 4.0 * PI * PI * fc * fc) / a[0];
+    
+    a[0] = 1.0;
 }
 
 
 #[allow(non_snake_case)]
 pub fn safe_IIR_LPF(fc  : c_double, Q:  c_double , a: &mut [c_double], b: &mut [c_double]){
+	assert_eq!(3, a.len());
+	assert_eq!(3, b.len());
   	let fc = (PI * fc).tan() / (2.0 * PI);
 
     a[0] = 1.0 + 2.0 * PI * fc / Q + 4.0 * PI * PI * fc * fc;
