63d0d83c8448e66aa8911da91f196ed277efbd96
hsjoihs <hs.ioling.hs@gmail.com>
2018-05-16 16:33:56 +0900

    ex8_6 successful

--- sound_programming/src/main.rs
+++ sound_programming/src/main.rs
@@ -51,7 +51,8 @@ fn main() {
 	ex8_2();
 	ex8_3();
 	// ex8_4(); // random
-	ex8_5();
+	// ex8_5(); // random
+	ex8_6();
 	ex10_4();
 }
 
@@ -1146,6 +1147,118 @@ fn ex8_5(){
   	}
   	wave_write_16bit_mono_safer2("ex8_5.wav", (&mut pcm_s, pcm_fs, pcm_bits, pcm_length));
 
+}
+
+fn square_wave(x: (&mut[c_double], usize), f0 : c_double, gain: c_double, offset: usize, duration : usize){
+	let mut s = vec![0.0;duration];
+	let (pcm_s, pcm_fs) = x;
+	/* 矩形波 */
+  	let t0 = (pcm_fs as f64 / f0) as usize; /* 基本周期 */
+  	let mut m = 0;
+  	for n in 0..duration {
+    	s[n] = if (m as f64) < t0 as f64 / 2.0
+    	{
+    	  1.0
+    	}
+    	else
+    	{
+    	  -1.0
+    	};
+    	
+    	m+=1;
+    	if m >= t0
+    	{
+    	  m = 0;
+    	}
+  	}
+  
+  	for n in 0..duration {
+    	s[n] *= gain;
+  	}
+  
+  	for n in 0..duration {
+    	pcm_s[offset + n] += s[n];
+  	}
+}
+
+fn triangle_wave(x: (&mut[c_double], usize), f0 : c_double, gain: c_double, offset: usize, duration : usize){
+	let mut s = vec![0.0;duration];
+	let (pcm_s, pcm_fs) = x;
+	/* 三角波 */
+	let t0 = (pcm_fs as f64 / f0) as usize; /* 基本周期 */
+	let mut m = 0;
+	for n in 0..duration {
+		s[n] = if (m as f64) < t0 as f64 / 2.0 {
+			-1.0 + 4.0 * m as f64 / t0 as f64
+		} else {
+			3.0 - 4.0 * m as f64 / t0 as f64
+		};
+		m+=1;
+		if m >= t0 {
+			m=0;
+		}
+	}
+	for n in 0..duration {
+		s[n] *= gain;
+	}
+	for n in 0..duration {
+		pcm_s[offset + n] += s[n];
+	}
+}
+
+fn white_noise(pcm_s: &mut[c_double], gain: c_double, offset: usize, duration: usize){
+	let mut s = vec![0.0;duration];	
+	let mut rng = rand::thread_rng();
+	/* 白色雑音 */
+	for n in 0..duration {
+		s[n] = rng.gen_range(-1.0, 1.0);
+	}
+	for n in 0..duration {
+		s[n] *= gain;
+	}
+	for n in 0..duration {
+		pcm_s[offset + n] += s[n];
+	}
+}
+
+#[allow(non_snake_case, unused_variables)]
+fn ex8_6(){
+	let pcm_fs = 44100; /* 標本化周波数 */
+    let pcm_bits = 16; /* 量子化精度 */
+    let pcm_length = 7000 * 16; /* 音データの長さ */
+    let mut pcm_s = vec![0.0; pcm_length]; /* 音データ */	
+  
+  	/* メロディパート */
+    square_wave((&mut pcm_s, pcm_fs), 659.26, 0.1, 7000 * 0, 6125); /* E5 */
+    square_wave((&mut pcm_s, pcm_fs), 659.26, 0.1, 7000 * 1, 6125); /* E5 */
+    square_wave((&mut pcm_s, pcm_fs), 659.26, 0.1, 7000 * 3, 6125); /* E5 */
+    square_wave((&mut pcm_s, pcm_fs), 523.25, 0.1, 7000 * 5, 6125); /* C5 */
+    square_wave((&mut pcm_s, pcm_fs), 659.26, 0.1, 7000 * 6, 6125); /* E5 */
+    square_wave((&mut pcm_s, pcm_fs), 783.99, 0.1, 7000 * 8, 6125); /* G5 */
+    square_wave((&mut pcm_s, pcm_fs), 392.00, 0.1, 7000 * 12, 6125); /* G4 */
+    
+    /* ベースパート */
+    triangle_wave((&mut pcm_s, pcm_fs), 146.83, 0.2, 7000 * 0, 6125); /* D3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 146.83, 0.2, 7000 * 1, 6125); /* D3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 146.83, 0.2, 7000 * 3, 6125); /* D3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 146.83, 0.2, 7000 * 5, 6125); /* D3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 146.83, 0.2, 7000 * 6, 6125); /* D3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 196.00, 0.2, 7000 * 8, 6125); /* G3 */
+    triangle_wave((&mut pcm_s, pcm_fs), 196.00, 0.2, 7000 * 12, 6125); /* G3 */
+
+	/* パーカッション */
+    white_noise(&mut pcm_s, 0.1, 7000 * 0, 4000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 2, 1000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 3, 4000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 5, 1000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 6, 4000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 8, 4000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 11, 4000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 13, 1000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 14, 1000);
+    white_noise(&mut pcm_s, 0.1, 7000 * 15, 1000); 
+
+    wave_write_16bit_mono_safer2("ex8_6.wav", (&mut pcm_s, pcm_fs, pcm_bits, pcm_length));
 }
 
 
