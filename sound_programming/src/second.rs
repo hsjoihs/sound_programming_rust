75e27e3801016d5c8b25c909babd7fb25471aa17
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-09-02 14:39:31 +0900

    Remove fraction; refactor

--- sound_programming/src/second.rs
+++ sound_programming/src/second.rs
@@ -26,69 +26,71 @@ enum NoteName {
 #[derive(Copy, Clone, Debug)]
 struct Pitch(NoteName, i32);
 impl Pitch {
-    pub fn get_pitch(self) -> f64 {
+    pub fn to_hertz(self) -> f64 {
         let Pitch(name, octave) = self;
         440. * 2f64.powf(1. / 12.).powi(name as i32 + 12 * (octave - 5))
     }
 }
 
+fn melody_font(mut pcm: &mut wave_utils::MonoPcm, unit_length: usize, counter: usize, pitch: f64, len: usize) {
+    square_wave(&mut pcm, pitch, 0.1, unit_length * counter, unit_length * len * 7 / 8)
+}
+
+fn render<F>(mut pcm: &mut wave_utils::MonoPcm, unit_length: usize, melody: &[(usize, Option<Pitch>)], melody_font: F)
+where F : Fn(&mut wave_utils::MonoPcm, usize, usize, f64, usize) -> (){
+    let mut counter = 0;
+    for (len, pitch) in melody {
+        if let Some(p) = pitch {
+            melody_font(&mut pcm, unit_length, counter, p.to_hertz(), *len);
+        }
+        counter += len;
+    }
+}
+
 pub fn second() {
-    let eighth_note_length = 8000; // number of samples per an eighth note
+    let unit_length = 8000; // number of samples per an eighth note
     let pcm_fs = 44100; /* 標本化周波数 */
-    let pcm_length = eighth_note_length * 16; /* 音データの長さ */
+    let pcm_length = unit_length * 16; /* 音データの長さ */
     let mut pcm = MonoPcm::new16(pcm_fs, pcm_length); /* 音データ */
 
-    let mut counter = 0;
     use self::NoteName::*;
-    for pitch in vec![
-        Some(Pitch(E, 5)),
-        Some(Pitch(E, 5)),
-        None,
-        Some(Pitch(E, 5)),
-        None,
-        Some(Pitch(C, 5)),
-        Some(Pitch(E, 5)),
-        None,
-        Some(Pitch(G, 5)),
-        None,
-        None,
-        None,
-        Some(Pitch(G, 4))
-    ] {
-        let freq = match pitch {
-            Some(a) => Some(a.get_pitch()),
-            None => None,
-        };
-        match freq {
-            None => {},
-            Some(f) => {
-                square_wave(&mut pcm, f, 0.1, eighth_note_length * counter, 7000);
-            }
-        }
-
-        counter += 1;
-    }
+    let melody = vec![
+        (1, Some(Pitch(E, 5))),
+        (1, Some(Pitch(E, 5))),
+        (1, None),
+        (1, Some(Pitch(E, 5))),
+        (1, None),
+        (1, Some(Pitch(C, 5))),
+        (1, Some(Pitch(E, 5))),
+        (1, None),
+        (1, Some(Pitch(G, 5))),
+        (1, None),
+        (1, None),
+        (1, None),
+        (1, Some(Pitch(G, 4)))
+    ];
+    render(&mut pcm, unit_length, &melody, melody_font);
 
     /* ベースパート */
-    triangle_wave(&mut pcm, 146.83, 0.2, eighth_note_length * 0, 7000); /* D3 */
-    triangle_wave(&mut pcm, 146.83, 0.2, eighth_note_length * 1, 7000); /* D3 */
-    triangle_wave(&mut pcm, 146.83, 0.2, eighth_note_length * 3, 7000); /* D3 */
-    triangle_wave(&mut pcm, 146.83, 0.2, eighth_note_length * 5, 7000); /* D3 */
-    triangle_wave(&mut pcm, 146.83, 0.2, eighth_note_length * 6, 7000); /* D3 */
-    triangle_wave(&mut pcm, 196.00, 0.2, eighth_note_length * 8, 7000); /* G3 */
-    triangle_wave(&mut pcm, 196.00, 0.2, eighth_note_length * 12, 7000); /* G3 */
+    triangle_wave(&mut pcm, 146.83, 0.2, unit_length * 0, 7000); /* D3 */
+    triangle_wave(&mut pcm, 146.83, 0.2, unit_length * 1, 7000); /* D3 */
+    triangle_wave(&mut pcm, 146.83, 0.2, unit_length * 3, 7000); /* D3 */
+    triangle_wave(&mut pcm, 146.83, 0.2, unit_length * 5, 7000); /* D3 */
+    triangle_wave(&mut pcm, 146.83, 0.2, unit_length * 6, 7000); /* D3 */
+    triangle_wave(&mut pcm, 196.00, 0.2, unit_length * 8, 7000); /* G3 */
+    triangle_wave(&mut pcm, 196.00, 0.2, unit_length * 12, 7000); /* G3 */
 
     /* パーカッション */
-    white_noise(&mut pcm, 0.1, eighth_note_length * 0, 4000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 2, 1000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 3, 4000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 5, 1000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 6, 4000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 8, 4000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 11, 4000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 13, 1000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 14, 1000);
-    white_noise(&mut pcm, 0.1, eighth_note_length * 15, 1000);
+    white_noise(&mut pcm, 0.1, unit_length * 0, 4000);
+    white_noise(&mut pcm, 0.1, unit_length * 2, 1000);
+    white_noise(&mut pcm, 0.1, unit_length * 3, 4000);
+    white_noise(&mut pcm, 0.1, unit_length * 5, 1000);
+    white_noise(&mut pcm, 0.1, unit_length * 6, 4000);
+    white_noise(&mut pcm, 0.1, unit_length * 8, 4000);
+    white_noise(&mut pcm, 0.1, unit_length * 11, 4000);
+    white_noise(&mut pcm, 0.1, unit_length * 13, 1000);
+    white_noise(&mut pcm, 0.1, unit_length * 14, 1000);
+    white_noise(&mut pcm, 0.1, unit_length * 15, 1000);
 
     wave_write_16bit_mono_safer3("ex8_6.wav", &pcm); 
 }
